b0a90094f4ba0aea98d8a7ec209e4f8cd1da11cc
TheiloT <theilo.terrisse@gmail.com>
2024-01-05 13:27:06 +0100

    Add function to detect TP and FP, working for synthetic dataset

--- cdlgr/model/dictionary.py
+++ cdlgr/model/dictionary.py
@@ -77,7 +77,6 @@ class Dictionary:
             # plt.show()
 
             self.dictionary /= np.linalg.norm(self.dictionary, axis=0)
-
     
     def recovery_error_interp(self, iteration, numOfsubgrids, save_plots=True):
         """
@@ -115,52 +114,6 @@ class Dictionary:
         dict1_interpolated, _ = dico.interpolate(numOfsubgrids, kind=self.config["model"]["cdl"]["interpolator_type"])
         numOfinterp = int(dict1_interpolated.shape[1]/filternum)
 
-        # if save_plots:
-        #     plt.close('all')
-        #     plt.figure()
-        #     for i in np.arange(filternum):
-        #         for j in np.arange(numOfinterp):
-        #             plt.plot(dict1_interpolated[:,i*numOfinterp+j], label=f"True {i} {j}")
-        #         plt.plot(dict2[:,i], label=f"Estimated {i}", linewidth=4)
-        #         plt.legend()
-        #         plt.savefig(f"dictionary-i2-{iteration:03}-{i:03}.png")
-        #     plt.close('all')
-        
-        # indices = np.zeros(filternum, dtype=int)
-        # for i in np.arange(filternum):
-        #     diff = 1
-        #     idx = 0
-        #     o_max = 0
-        #     for j in np.arange(numOfinterp):
-        #         temp = []
-        #         for o in offsets:
-        #             temp.append(np.dot(np.roll(dict1_interpolated[:,i*numOfinterp+j],o),dict2[:,i]))
-
-        #         temp_diff = 1-np.power(np.max(temp), 2)
-                
-        #         print(temp_diff)
-
-        #         if temp_diff<diff:
-        #             idx = j
-        #             o_max = np.argmin(temp_diff)
-        #             diff = temp_diff
-
-        #     err_distance[i] = np.sqrt(diff)
-        #     indices[i] = idx
-
-        #     if save_plots:
-        #         plt.figure(figsize=(10,5))
-        #         times = np.arange(dict2.shape[0])/self.fs
-        #         # plt.plot(times,dict1_interpolated[:,i*numOfinterp+idx], label="True")
-        #         true_label = "True" if self.config["model"]["cdl"]["interpolate"] == "0" else fr"True (interpolated by ${idx}\,\Delta_K$)"
-        #         plt.plot(times, np.roll(dict1_interpolated[:,i*numOfinterp+idx],o_max), label=true_label, marker='x')
-        #         plt.plot(times, dict2[:,i], label="Estimated", marker='+') 
-        #         plt.xlabel("Time")
-        #         plt.ylabel("Amplitude (normalized)")
-        #         plt.title(f"Element {i} - Error {err_distance[i]:.3f} - Best offset {-o_max}")
-        #         plt.legend()
-        #         plt.savefig(f"dictionary-i-{iteration:03}-{i:03}.png")            
-        
         indices = np.zeros(filternum, dtype=int)
         for unit in np.arange(filternum):
             estimated_dict = dict2[:,unit]/np.linalg.norm(dict2[:,unit])
@@ -181,7 +134,7 @@ class Dictionary:
                 plt.figure(figsize=(10,5))
                 times = np.arange(dict2.shape[0])/self.fs
                 true_label = "True" if self.config["model"]["cdl"]["interpolate"] == "0" else fr"True (interpolated by ${idx}\times\Delta_K$)"
-                plt.plot(times, np.roll(dict1_interpolated[:,unit*numOfinterp+idx],o), label=true_label, marker='x')
+                plt.plot(times, np.roll(dict1_interpolated[:,unit*numOfinterp+idx],best_offset), label=true_label, marker='x')
                 plt.plot(times, estimated_dict, label="Estimated", marker='+') 
                 plt.xlabel("Time")
                 plt.ylabel("Amplitude (normalized)")
