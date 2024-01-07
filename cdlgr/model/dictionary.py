0f4ffb2272a92c13f1bcec850a81185e86441b4d
Florent Pollet <flo1.raymond@gmail.com>
2024-01-08 00:03:59 +0100

    progress real experiments notebook

--- cdlgr/model/dictionary.py
+++ cdlgr/model/dictionary.py
@@ -34,7 +34,7 @@ class Dictionary:
         ms_before = length_ms/2 if length_ms is not None else 1.0
         ms_after = length_ms/2 + 1000/self.fs if length_ms is not None else 2.0
         wv = si.extract_waveforms(self.dataset.recording, self.dataset.sorting_true, max_spikes_per_unit=2500, ms_before=ms_before, ms_after=ms_after, mode="memory")
-        templates = wv.get_all_templates()  # Shape (num_units, num_samples, num_channels)
+        templates = wv.get_all_templates(mode='median').astype(float)  # Shape (num_units, num_samples, num_channels)
 
         # remove code duplication
         self.true_dictionary = np.zeros_like(self.dictionary)
